76e6b82e46d2f4096937317e2d3e9e019df6fb3d
Fimbelowski <chrfimbel@gmail.com>
2018-09-02 23:43:22 -0700

    Added functionality for opening and closing the modal overlay.

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ window.onload = function() {
   vm = new Vue({
     el: '#v-app',
     data: {
-      
+      modalActive: false
+    },
+    methods: {
+      openModal: function() {
+        this.modalActive = true;
+      },
+      closeModal: function() {
+        this.modalActive = false;
+      }
     }
   });
 }
