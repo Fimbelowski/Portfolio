fbd2ae6e43dcaee2fda2eb330c2977d387f52ecc
Fimbelowski <chrfimbel@gmail.com>
2018-09-07 15:38:29 -0700

    Added functionality for updating the window's location hash upon clicking on a tab. Also added the functionality to load specifically into a specific tab given a URL with a valid hash.

--- main.js
+++ main.js
@@ -18,7 +18,29 @@ window.onload = function() {
       },
       updateActiveTab: function(targetTab) {
         this.activeTab = targetTab;
+        this.updateLocationHash(targetTab);
       },
+      updateLocationHash: function(newHash) {
+        window.location.hash = newHash;
+      },
+      checkForLocationHash: function() {
+        var hash = window.location.hash.slice(1);
+        // If the window already has a valid location hash upon loading...
+        if(hash === 'about' ||
+           hash === 'projects' ||
+           hash === 'tools' ||
+           hash === 'contact') {
+             // Set the active tab equal to the existing hash
+             this.updateActiveTab(hash);
+           } else {
+        // Set the hash and the active tab equal to the default ('about')
+        this.updateLocationHash('about');
+        this.updateActiveTab('about');
+        }
+      }
+    },
+    mounted: function() {
+      this.checkForLocationHash();
     }
   });
 }
