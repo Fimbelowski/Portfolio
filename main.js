dad007f2ccd0ce0e512905ccd160082de9b2b6d1
Fimbelowski <chrfimbel@gmail.com>
2018-09-18 23:44:59 -0700

    Implemented tab transitions. Updated a number of other things in the process.

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ window.onload = function() {
     el: '#v-app',
     data: {
       modalActive: false,
-      activeTab: 'about',
-      fadeIn: '',
-      fadeOut: ''
+      activeButton: '',
+      activeTab: '',
+      mainContentState: 'fade-in',
+      updateDelay: 200
     },
     methods: {
       openModal: function() {
@@ -18,37 +19,75 @@ window.onload = function() {
           this.modalActive = false;
         }
       },
+      updateActiveButton: function(targetButton) {
+        // If the button being pressed is already active...
+        if(this.activeButton === targetButton) {
+          // Do nothing
+        } else {
+          // Activate the new button
+          this.activeButton = targetButton;
+        }
+      },
       updateActiveTab: function(targetTab) {
         // If the tab being activated is already active...
         if(this.activeTab === targetTab) {
           // Do nothing
         } else {
-          // Update the active tab appropriately
+          // Activate the target tab
           this.activeTab = targetTab;
-          this.updateLocationHash(targetTab);
         }
       },
-      updateLocationHash: function(newHash) {
-        window.location.hash = newHash;
+      toggleMainContentFade: function() {
+        if(this.mainContentState === 'fade-in') {
+          this.mainContentState = 'fade-out';
+        } else {
+          this.mainContentState = 'fade-in';
+        }
       },
-      checkForLocationHash: function() {
+      buttonClicked: function(targetButton) {
+        // Update the active button
+        this.updateActiveButton(targetButton);
+
+        // Fade out the main content
+        this.toggleMainContentFade();
+
+        // After a delay, update the active tab, the location hash and fade in the main content
+        setTimeout(this.updateActiveTab, this.updateDelay, targetButton);
+        setTimeout(this.updateLocationHash, this.updateDelay, targetButton);
+        setTimeout(this.toggleMainContentFade, this.updateDelay);
+      },
+      locationHashIsValid: function() {
         var hash = window.location.hash.slice(1);
-        // If the window already has a valid location hash upon loading...
+
         if(hash === 'about' ||
            hash === 'projects' ||
            hash === 'tools' ||
            hash === 'contact') {
-             // Set the active tab equal to the existing hash
-             this.updateActiveTab(hash);
+             return true;
            } else {
-        // Set the hash and the active tab equal to the default ('about')
-        this.updateLocationHash('about');
-        this.updateActiveTab('about');
-        }
+             return false;
+           }
+      },
+      updateLocationHash: function(newHash) {
+        window.location.hash = newHash;
       }
     },
     mounted: function() {
-      this.checkForLocationHash();
+      // If the window already has a valid location hash...
+      if(this.locationHashIsValid()) {
+        var existingHash = window.location.hash.slice(1);
+
+        // Set the active tab equal to the existing hash
+        this.updateActiveTab(existingHash);
+
+        // Set the active button equal to the existing hash
+        this.updateActiveButton(existingHash);
+      } else {
+        // Set the hash, active tab, and active button to the default (about)
+        this.updateLocationHash('about');
+        this.updateActiveButton('about');
+        this.updateActiveButton('about');
+      }
     }
   });
 }
