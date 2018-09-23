0c0a6fe172f5b79426291336f23f016b69e2332a
Fimbelowski <chrfimbel@gmail.com>
2018-09-23 09:43:20 -0700

    Finished setting up SASS tasks with Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function() {
   return gulp.src('./scss/*.scss')
   .pipe(sass().on('error', sass.logError))
+  .pipe(cleanCSS())
   .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function() {
-  gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', function() {
-
-});
+gulp.task('default', ['sass:watch']);
