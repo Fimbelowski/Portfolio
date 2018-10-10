acb01e6317af4953267d366b4cecefb811641a0e
Fimbelowski <chrfimbel@gmail.com>
2018-10-09 20:27:35 -0700

    Began to flesh out modal box functionality and styling.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,29 @@
 var vm;
 
-// window.onload = function() {
-//   vm = new Vue({});
-// }
+window.onload = function() {
+  vm = new Vue({
+    el: '#projects',
+    data: {
+      modalActive: false,
+      activeModalContent: {
+        title: ''
+      },
+      projectPreviews: [{
+        title: "What's WR?",
+        subtitle: "Find a random speedrun world record to watch!"
+      }]
+    },
+    methods: {
+      activateModal: function(contentIndex) {
+        this.toggleModal();
+        this.setActiveModalContent(contentIndex);
+      },
+      toggleModal: function() {
+        this.modalActive = !this.modalActive;
+      },
+      setActiveModalContent: function(contentIndex) {
+        this.activeModalContent = this.projectPreviews[contentIndex];
+      }
+    }
+  });
+}
