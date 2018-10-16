d3250ce5b11d3e36896e2c59e49d944305b2f34c
Fimbelowski <chrfimbel@gmail.com>
2018-10-16 15:30:40 -0700

    Replaced placeholder text for the What's WR project information.

--- src/js/main.js
+++ src/js/main.js
@@ -21,8 +21,9 @@ window.onload = function() {
         gallery: ['dist/images/projects/whats-wr/gallery-1.png',
                   'dist/images/projects/whats-wr/gallery-2.png'
         ],
-        desc: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+        desc: ['"What\'s WR" is a site that will find a random speedrun world record video for the user to watch. Speedruns are the act of completing a video game as fast as possible within the rules and regulations set by the speedrunning community for that particular game. This helps users find obscure games or runners they would have otherwise never known about, while also providing entertainment via a world record video.',
+              '"What\'s WR" works mainly through consuming Speedrun.com\'s API. The site must first find out how many games are currently supported on Speedrun.com and then fetches a random few games from the site as a starting point. The site then picks a random game from the set and starts reviewing the game\'s different categories. A category will have to pass a set of criteria before being deemed fit to show to the user.',
+              'After a game and category are deemed fit for viewing, the site makes a few more calls to the API concurrently to retrieve auxiliary information such as information about the game and category, information about the runner(s), and also links to the runner(s) social media accounts.'],
         uri: 'https://fimbelowski.github.io/Whats-WR/'
       }]
     },
