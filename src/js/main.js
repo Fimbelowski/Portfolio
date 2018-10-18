33726275c28c99fe4676668d8b4acf2d34c9b479
Fimbelowski <chrfimbel@gmail.com>
2018-10-17 18:54:32 -0700

    Added buttons to view a project on Github in both the preview overlay and the modal overlay.

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,8 @@ window.onload = function() {
         subtitle: '',
         gallery: ['', ''],
         desc: '',
-        uri: ''
+        uri: '',
+        github: ''
       },
         {
         title: "What's WR?",
@@ -24,7 +25,8 @@ window.onload = function() {
         desc: ['"What\'s WR" is a site that will find a random speedrun world record video for the user to watch. Speedruns are the act of completing a video game as fast as possible within the rules and regulations set by the speedrunning community for that particular game. This helps users find obscure games or runners they would have otherwise never known about, while also providing entertainment via a world record video.',
               '"What\'s WR" works mainly through consuming Speedrun.com\'s API. The site must first find out how many games are currently supported on Speedrun.com and then fetches a random few games from the site as a starting point. The site then picks a random game from the set and starts reviewing the game\'s different categories. A category will have to pass a set of criteria before being deemed fit to show to the user.',
               'After a game and category are deemed fit for viewing, the site makes a few more calls to the API concurrently to retrieve auxiliary information such as information about the game and category, information about the runner(s), and also links to the runner(s) social media accounts.'],
-        uri: 'https://fimbelowski.github.io/Whats-WR/'
+        uri: 'https://fimbelowski.github.io/Whats-WR/',
+        github: 'https://github.com/Fimbelowski/Whats-WR'
       }]
     },
     methods: {
