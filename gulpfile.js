45ea5b7e84260f064d5ced175a2e23198323ead3
unknown <ChrFimbel@gmail.com>
2019-02-10 14:35:54 -0700

    Added basic scss architecture. Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,19 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
-    CleanCSS = require('clean-css');
+    cleanCSS = require('gulp-clean-css');
 
-// Define the default task and add the watch task to it.
-gulp.task('default', ['watch']);
-
-// Configure the jshint task.
-gulp.task('jshint', function() {
-    return gulp.src('source/scripts/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-// Configure the concat task.
-gulp.task('concat-js', function() {
-    return gulp.src('source/scripts/**/(.js')
-    .pipe(concat('app.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'));
-});
-
-// Configure the SASS task.
+// Configure build-css task.
 gulp.task('build-css', function() {
     return gulp.src('source/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(concat('styles.css'))
-    .pipe(CleanCSS())
-    .pipe(gulp.dest('dist/css/'));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css'));
 });
 
-// Configure which files to watch and what tasks to use on those file changes.
-gulp.task('watch', function() {
-    gulp.watch('source/scripts/**/*.js', ['jshint']);
-    gulp.watch('source/scss/**/*.scss', ['build-css']);
+// Configure a task to watch scss files and run build-scss on any changes.
+gulp.task('build-css:watch', function() {
+    gulp.watch('source/scss/**/*.scss', gulp.series('build-css'));
 });
+
+// Configure the default task.
+gulp.task('default', gulp.series('build-css', 'build-css:watch'));
